2a1bddd4d3b69aa2bb373859aa3010b940dfc23e
Frank Mazzarella <mazzarella.frank@gmail.com>
2020-05-23 14:40:44 -0400

    adding express and launch config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-console.log('Hello, World!')!
+import compression from 'compression';
+import express from 'express';
+import fs from 'fs';
+import os from 'os';
+
+handleErrors();
+initServer();
+
+// TODO: verify these log correctly
+function handleErrors() {
+  process.on('unhandledRejection', (reason, promise) => {
+    const promiseStr = JSON.stringify(promise);
+    const reasonStr = JSON.stringify(reason);
+    console.error(`process terminating due to unhandled rejection at: ${promiseStr}, reason: ${reasonStr}`);
+    process.exit(1);
+  });
+
+  process.on('uncaughtException', (err) => {
+    console.error(`process terminating due to uncaught exception: ${err}`);
+    process.exit(1);
+  });
+}
+
+function initServer() {
+  const packageJson = JSON.parse(fs.readFileSync('package.json').toString());
+  const app = express();
+  const port = 3000;
+  app.use(compression());
+  app.get('/healthcheck', (req, res) => res.send({ uptime: process.uptime() }));
+  app.listen(port, () => {
+    console.log(`${packageJson.name} v${packageJson.version} has started on port ${port} [host: ${os.hostname}]`);
+  });
+}
